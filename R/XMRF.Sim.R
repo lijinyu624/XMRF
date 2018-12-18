28347443aafddaa94690f25867c9c17749e68231
lijinyu624 <lijinyu624@gmail.com>
2018-12-18 12:39:29 -0600

    Update XMRF.Sim.R

--- R/XMRF.Sim.R
+++ R/XMRF.Sim.R
@@ -44,7 +44,7 @@ function(n=100, p=50, model="LPGM", graph.type="scale-free"){
 	#
 	if(model == "ISM"){
 		theta = 0.01
-		tau = 0.05
+		tau = 50
 		maxit = 1000
 		
 		X <- matrix(rbinom(n*p, 1, 0.6), nrow=n, ncol=p)
